58f3840bef7011837d92dfc27da49e62858ebf08
yangsej <yangsej@gmail.com>
2020-12-30 20:47:49 +0900

    Update Logs

---
+++ Logs/2020-06-13 16-20-48/CifarNet.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+# (1) 내용: Training a classifier using CNN
+
+# (1) Classification 정확도를 향상시키고,
+# (2) 각 epoch마다 test set에 대한 정확도를 구하기,
+# (3) 과제보고서 작성하여 제출하기
+# (4) 소스코드도 함께 제출 함
+
+# CIFAR-10 은 10개 클래스 총 6만장의 데이터셋으로 이루어져 있으며, training dataset 5만장, test dataset 1만장으로 구성되어 있습니다.
+# 이번 과제는 10개 class를 분류하는 baseline model을 수정해서 학습시켜 baseline model 보다 높은 정확도를 내는 모델을 만들고 학습하는 것 입니다.
+# model을 수정하거나 epoch 수를 늘리는 등 수업시간에 배운 내용을 바탕으로 정확도를 높이고, 어떤 방법으로 validation 정확도가 얼마나 증가 했는지 보고서로 작성해서 제출하세요.
+# 단, data augmentation, learning rate scheduler는 사용하지 마세요.
+
+# 제출할 파일은 총 3가지로 소스파일, model weights파일, 보고서 입니다. 보고서에 반드시 들어갈 내용으로는 어떻게 성적을 향상시켰는지, 그리고 참고 문헌입니다.
+# 보고서 분량은 최대 7페이지를 넘어가지 않도록 작성해주세요.
+# 이번 과제와 관련하여 궁금한 점이 있으면 과제 QnA 게시판 을 통해 질문해주세요. 이번 과제는 과제 30%중 절반인 15%를 차지합니다. 많은 비중을 차지하는 만큼 잘 작성하여 제출해 주시기 바랍니다.
+"""
+Created on Sun May 31 20:38:03 2020
+
+@author: RML
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CifarNet(nn.Module):
+    def __init__(self):
+        super(CifarNet, self).__init__()
+        dropout_p = 0.1
+        self.conv_layer = nn.Sequential(
+            # Conv Layer block 1
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout2d(p=dropout_p),
+
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout2d(p=dropout_p),
+
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout2d(p=dropout_p),
+        )
+
+        self.fc_layer = nn.Sequential(
+            nn.Linear(4096, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout_p),
+            nn.Linear(1024, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout_p),
+            nn.Linear(512, 10),
+        )
+
+        self.fc_layer.apply(self.__init_weight__)
+
+    def __init_weight__(self, m: nn.Module):
+        if type(m) == nn.Linear:
+            nn.init.kaiming_uniform_(m.weight)
+
+    def forward(self, x):
+        # conv layers
+        x = self.conv_layer(x)
+        
+        # flatten
+        x = x.view(x.size(0), -1)
+        
+        # fc layer
+        x = self.fc_layer(x)
+
+        return x
+
+def _cifarnet(pretrained=False, path=None):
+    model = CifarNet()
+    if pretrained:
+        state_dict = torch.load(path)
+        model.load_state_dict(state_dict)
+    return model
---
+++ Logs/2020-06-14 05-31-57/train_cifar10.py
@@ -0,0 +1,224 @@
+# -*- coding: utf-8 -*-
+import argparse
+
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+import CifarNet as cfn
+# import DPN
+
+import os
+import sys
+import shutil
+# import winsound
+from datetime import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--test", action="store_true")
+parser.add_argument("--show_data", action="store_true")
+
+# global data
+classes = (
+    "plane",
+    "car",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+)
+
+
+DATA_PATH = "./data"
+
+NUM_WORKERS = 2
+EPOCHS = 50
+BATCH_SIZE = 1024
+WEIGHT_DECAY = 1e-3
+LEARNING_RATE = 1e-3
+
+now = datetime.now()
+now_str = now.strftime("%F %H-%M-%S")
+DIR_PATH = "./%s" % (now_str)
+
+OUT_FILE = None
+if __name__ == "__main__":
+    if not os.path.exists(DIR_PATH):
+        os.mkdir(DIR_PATH)
+    shutil.copy2("./CifarNet.py", "%s/CifarNet.py" % DIR_PATH)
+    shutil.copy2("./train_cifar10.py", "%s/train_cifar10.py" % DIR_PATH)
+    OUT_FILE = open("%s/cifar_net.txt" % DIR_PATH, "w")
+MODEL_PATH = "%s/cifar_net.pth" % DIR_PATH 
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def main():
+    args = parser.parse_args()
+    if args.test:
+        test_dataloader = cifar10_dataloader(
+            train=False, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS
+        )
+        # model = DPN.DPN92().to(device)
+        model = cfn._cifarnet(pretrained=args.test, path=MODEL_PATH).to(device)
+        test(test_dataloader, model, args.show_data)
+        return
+
+    if args.show_data:
+        dataloader = cifar10_dataloader(
+            train=False, batch_size=BATCH_SIZE, shuffle=True, num_workers=0
+        )
+        show_data(dataloader)
+        return
+
+    train_dataloader = cifar10_dataloader(
+        train=True, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS
+    )
+    test_dataloader = cifar10_dataloader(
+        train=False, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS
+    )
+
+    # model = DPN.DPN92().to(device)
+    model = cfn._cifarnet().to(device)
+    criterion = nn.CrossEntropyLoss().to(device)
+    optimizer = optim.SGD(
+        model.parameters(), lr=LEARNING_RATE, momentum=0.9, weight_decay=WEIGHT_DECAY
+    )
+
+    for epoch in range(EPOCHS):
+        print("Epoch %i" % (epoch+1), file=OUT_FILE)
+
+        train(train_dataloader, model, criterion, optimizer, epoch)
+        torch.save(model.state_dict(), MODEL_PATH)
+
+        test(train_dataloader, model, args.show_data)
+        test(test_dataloader, model, args.show_data)
+
+
+def cifar10_dataloader(
+    root=DATA_PATH,
+    train=True,
+    transform=None,
+    shuffle=False,
+    download=True,
+    batch_size=4,
+    num_workers=0,
+):
+
+    if transform is None:
+        transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
+
+    dataset = torchvision.datasets.CIFAR10(
+        root=root, train=train, transform=transform, download=download
+    )
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, num_workers=num_workers, shuffle=shuffle
+    )
+
+    return dataloader
+
+
+def train(dataloader, model, criterion, optimizer, epoch):
+    running_loss = 0.0
+    for i, data in enumerate(dataloader):
+        images, labels = data[0].to(device), data[1].to(device)
+        logit = model(images)
+        loss = criterion(logit, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 2000 == 1999:
+            print(running_loss / 2000.0, file=OUT_FILE)
+            running_loss = 0.0
+        elif i == len(dataloader)-1:
+            print(running_loss / float(len(dataloader)), file=OUT_FILE)
+
+
+def test(dataloader, model, show_data):
+    if show_data:
+        dataiter = iter(dataloader)
+        images, labels = dataiter.next()
+        # show images
+        imshow(torchvision.utils.make_grid(images))
+        output = model(images.to(device))
+        _, predicted = torch.max(output, 1)
+        print(
+            "GT", " ".join("%6s" % classes[labels[j]] for j in range(4)), file=OUT_FILE
+        )
+        print(
+            "PT",
+            " ".join("%6s" % classes[predicted[j]] for j in range(4)),
+            file=OUT_FILE,
+        )
+        print(file=OUT_FILE)
+
+    correct = 0
+    total = 0
+    class_correct = list(0.0 for i in range(10))
+    class_total = list(0.0 for i in range(10))
+    with torch.no_grad():
+        for data in dataloader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+    print(
+        "Accuracy : %d %%"
+        % (100 * correct / total),
+        file=OUT_FILE,
+    )
+    print(file=OUT_FILE)
+    for i in range(10):
+        print(
+            "Accuracy of %5s : %2d %%"
+            % (classes[i], 100 * class_correct[i] / class_total[i] if class_total[i] else 0),
+            file=OUT_FILE,
+        )
+    print(file=OUT_FILE)
+
+
+def show_data(dataloader):
+    dataiter = iter(dataloader)
+    images, labels = dataiter.next()
+
+    # show images
+    imshow(torchvision.utils.make_grid(images))
+
+    # print labels
+    print(" ".join("%10s" % classes[labels[j]] for j in range(4)), file=OUT_FILE)
+
+
+def imshow(img: torch.Tensor):
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+    # winsound.Beep(6000, 5000)
